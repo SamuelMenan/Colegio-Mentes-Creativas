257362ad3e225432fb959b0fdfe28e3d97cfd053
SamuelMenan <sam100uel@gmail.com>
2025-11-10 02:16:00 -0500

    chore(eslint): add ESM eslint.config.js alongside eslint.config.cjs

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+  overrides: [
+    {
+      files: ['*.cjs'],
+      parserOptions: { sourceType: 'script' },
+      env: { node: true },
+    },
+    {
+      files: ['*.config.js', '*.js'],
+      parserOptions: { sourceType: 'module' },
+      env: { node: true },
+    },
+  ],
+};
