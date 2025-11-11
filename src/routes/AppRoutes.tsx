f1fd278f423f730ef2c6f195cc9177a21f901f09
SamuelMenan <sam100uel@gmail.com>
2025-11-11 07:21:45 -0500

    chore(routes): add PlanetaTierra route and keep existing routes intact

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import Layout from "../components/Layout";
 import Home from "../views/Home";
 import SistemaSolar from "../views/SistemaSolar";
 import ConstruccionConBloques3D from "../views/ConstruccionConBloques3D";
+import PlanetaTierra from "../views/PlanetaTierra";
 
 export default function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="sistema-solar" element={<SistemaSolar />} />
+  <Route path="planeta-tierra" element={<PlanetaTierra />} />
         <Route path="construccion-bloques3D" element={<ConstruccionConBloques3D />} />
       </Route>
     </Routes>
